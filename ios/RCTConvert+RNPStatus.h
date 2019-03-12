b57234b951d3fc50fd7d82ff6e0fcb12b27d5142
kimmanuel <kimberlie.manuel@yahoo.com>
2019-03-12 17:00:58 +0800

    Check Location Permission Only

--- ios/RCTConvert+RNPStatus.h
+++ ios/RCTConvert+RNPStatus.h
@@ -23,18 +23,6 @@ static NSString* RNPStatusRestricted = @"restricted";
 typedef NS_ENUM(NSInteger, RNPType) {
     RNPTypeUnknown,
     RNPTypeLocation,
-    RNPTypeCamera,
-    RNPTypeMicrophone,
-    RNPTypePhoto,
-    RNPTypeContacts,
-    RNPTypeEvent,
-    RNPTypeReminder,
-    RNPTypeBluetooth,
-    RNPTypeNotification,
-    RNPTypeBackgroundRefresh,
-    RNPTypeSpeechRecognition,
-    RNPTypeMediaLibrary,
-    RNPTypeMotion
 };
 
 @interface RCTConvert (RNPStatus)
--- ios/RCTConvert+RNPStatus.m
+++ ios/RCTConvert+RNPStatus.m
@@ -11,19 +11,6 @@
 @implementation RCTConvert (RNPStatus)
 
 RCT_ENUM_CONVERTER(RNPType, (@{ @"location" : @(RNPTypeLocation),
-                                @"camera" : @(RNPTypeCamera),
-                                @"microphone" : @(RNPTypeMicrophone),
-                                @"photo" : @(RNPTypePhoto),
-                                @"contacts" : @(RNPTypeContacts),
-                                @"event" : @(RNPTypeEvent),
-                                @"reminder" : @(RNPTypeReminder),
-                                @"bluetooth" : @(RNPTypeBluetooth),
-                                @"notification" : @(RNPTypeNotification),
-                                @"backgroundRefresh": @(RNPTypeBackgroundRefresh),
-                                @"speechRecognition": @(RNPTypeSpeechRecognition),
-                                @"mediaLibrary": @(RNPTypeMediaLibrary),
-                                @"motion": @(RNPTypeMotion)
-                                }),
                                 RNPTypeUnknown, integerValue)
 
 @end
